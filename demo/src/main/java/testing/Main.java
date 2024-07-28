a6d9d91abd9568f48c2959b72f7f896884de1536
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-07-29 00:06:17 +0700

    feat: Upload code

--- demo/src/main/java/testing/Main.java
+++ demo/src/main/java/testing/Main.java
@@ -3,28 +3,25 @@ package testing;
 public class Main {
 
     public static String checkQuadrant(int x, int y) {
-        String result = "";
-        if (x > 1000 || y > 1000 || x < -1000 || y < -1000) {
-            result = "invalid";
-        } else if (x == 0 && y == 0) {
-            result = "1 2 3 4";
-        } else if (x == 0 && y > 0) {
-            result = "1 2";
-        } else if (x == 0 && y < 0) {
-            result = "3 4";
-        } else if (x > 0 && y == 0) {
-            result = "1 4";
-        } else if (x < 0 && y == 0) {
-            result = "2 3";
-        } else if (x > 0 && y > 0) {
-            result = "1";
-        } else if (x < 0 && y > 0) {
-            result = "2";
-        } else if (x < 0 && y < 0) {
-            result = "3";
-        } else {
-            result = "4";
-        }
-        return result;
+        if (x > 1000 || y > 1000 || x < -1000 || y < -1000)
+            return "invalid";
+        else if (x == 0 && y == 0)
+            return "1 2 3 4";
+        else if (x == 0 && y > 0)
+            return "1 2";
+        else if (x == 0 && y < 0)
+            return "3 4";
+        else if (x > 0 && y == 0)
+            return "1 4";
+        else if (x < 0 && y == 0)
+            return "2 3";
+        else if (x > 0 && y > 0)
+            return "1";
+        else if (x < 0 && y > 0)
+            return "2";
+        else if (x < 0 && y < 0)
+            return "3";
+        else
+            return "4";
     }
 }
